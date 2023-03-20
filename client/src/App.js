7a84feb041744bd92dc077d85fd10d79b84e4359
Thomas <thomasjji14@gmail.com>
2023-03-20 13:41:32 -0400

    added very basic login page

--- client/src/App.js
+++ client/src/App.js
@@ -1,3 +1,33 @@
-const App = () => <div>Login page</div>
+import { useState } from 'react'
+
+const App = () => {
+  const [usernameText, setUsername] = useState('yourUsername')
+  const [passwordText, setPassword] = useState('yourPassword')
+
+  const submit = (event) => {
+    event.preventDefault()
+    console.log(`${usernameText} | ${passwordText}`)
+  }
+
+  const handleUsernameChange = (event) => {
+    setUsername(event.target.value)
+  }
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value)
+  }
+
+  return <div>
+            <h1>Login page</h1>
+            <form onSubmit={submit}>
+              <div>Username:</div>
+              <input value={usernameText} onChange={handleUsernameChange}/><br/>
+              <div>Password:</div>
+              <input value={passwordText} onChange={handlePasswordChange}/><br/>
+              <button type="submit">Login</button>
+            </form>
+         </div>
+
+}
 
 export default App;
