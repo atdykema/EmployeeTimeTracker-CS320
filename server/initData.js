31b58a8bce494766b02a9d59e66fa04138c4bdda
Andrew Lin <unseenmocha@gmail.com>
2023-03-29 19:27:38 -0400

    handled data edge cases

--- server/initData.js
+++ server/initData.js
@@ -3,34 +3,47 @@ const fs = require('fs'); // Node.js File System module
 const lunchRock = 'mockData/LunchRock_LLC-time-entries.json';
 const nightOwls = 'mockData/Night_Owls_Inc-time-entries.json';
 const onionTech = 'mockData/Onion_Technology-time-entries.json';
+const gizmoGram = 'mockData/GizmoGram-time-entries.json';
+
+
+const gizmoGramId = 3;
+const nightOwlsId = 1;
+const onionTechId = 4;
+const lunchRockId = 2;
 
 // Read the input JSON file
-const lunchRockData = JSON.parse(fs.readFileSync(lunchRock));
-const nightOwlsData= JSON.parse(fs.readFileSync(nightOwls));
-const onionTechData = JSON.parse(fs.readFileSync(onionTech));
+let lunchRockData = JSON.parse(fs.readFileSync(lunchRock));
+let nightOwlsData= JSON.parse(fs.readFileSync(nightOwls));
+let onionTechData = JSON.parse(fs.readFileSync(onionTech));
+let gizmoGramData = JSON.parse(fs.readFileSync(gizmoGram));
 
 // Reformat the JSON data
 
 const HMSTimeToHours = (timeEntry) => {
     let inTime = timeEntry.clockedIn.split(':').map(val=>parseInt(val));
     let outTime = timeEntry.clockedOut.split(':').map(val=>parseInt(val));
-    let hrsWorked = outTime[0]-inTime[0] + (1/60)*(outTime[1]-inTime[1]) + (1/3600)*(outTime[2]-inTime[2]);
-    return Math.abs(Number(hrsWorked.toFixed(2)));
+    //value from 0-24 in hours that represents when in the day they clocked in/out
+    let inHrs = inTime[0] + (1/60)*inTime[1] + (1/3600)*inTime[2];
+    let outHrs = outTime[0] + (1/60)*outTime[1] + (1/3600)*outTime[2];
+    //simply put did they work past midnight
+    let nightShift = inHrs > outHrs;
+    //hours elapsed depending on if its a night shift
+    let hrsWorked = nightShift? 24 - inHrs + outHrs: outHrs - inHrs;
+    //if worked through daylight savings shift (only one relevant throughout data is 2022-11-06) then add lost hour
+    hrsWorked += nightShift && timeEntry.date === "2022-11-06" && outHrs >= 1? 1 : 0;
+
+    return Number(hrsWorked.toFixed(2));
 };
 
 const millisecondDateToHours = (timeEntry) => {
     let dateIn = new Date(timeEntry.clockedInEpochMillisecond);
-    let dateOut = new Date(timeEntry.clockedOutEpochMillisecond);
     let year = dateIn.getFullYear();
     let month = ("0" + (dateIn.getMonth() + 1)).slice(-2); // add leading zero to month
     let day = ("0" + dateIn.getDate()).slice(-2); // add leading zero to day
-    let hours = dateOut.getHours()-dateIn.getHours();
-    let minutes = dateOut.getMinutes()-dateIn.getMinutes();
-    let seconds = dateOut.getSeconds()-dateIn.getSeconds();
-    hrsWorked = hours + (1/60)*minutes + (1/3600)*seconds;
+    hrsWorked = (timeEntry.clockedOutEpochMillisecond - timeEntry.clockedInEpochMillisecond)/3600000;
     return ({
         date: `${year}-${month}-${day}`,
-        hoursWorked: Math.abs(Number(hrsWorked.toFixed(2))),
+        hoursWorked: Number(hrsWorked.toFixed(2)),
     });
 };
 
@@ -57,25 +70,50 @@ const formatMilisecondTime = (timeData) => {
             timeEntries: employeeTimeObj.timeEntries.map(timeEntry => millisecondDateToHours(timeEntry)),            
         })
     })
-}
+};
 
-const lunchRockReformatted = formatHMSTimeData(lunchRockData);
-const nightOwlsReformatted = formatHMSTimeData(nightOwlsData);
-const onionTechDataReformatted = formatMilisecondTime(onionTechData);
+//so we can actually use the companyID
+const changeCompanyID = (employeeTimeObj, companyId) => ({
+    ...employeeTimeObj, companyID: companyId,
+});
+
+//assumes data has been converted to hrsWorked
+const combineDuplicateDays = (employeeTimeObj) => {
+    let newTimeEntries = [];
+    employeeTimeObj.timeEntries.forEach(timeEntry => {
+        let existing = newTimeEntries.find(newTimeEntry=>newTimeEntry.date===timeEntry.date)
+        if (existing !== undefined) {
+            existing.hoursWorked += timeEntry.hoursWorked;
+            existing.hoursWorked = Number(existing.hoursWorked.toFixed(2));
+        } else {
+            newTimeEntries.push(timeEntry);
+        }
+    })
+    return ({
+        ...employeeTimeObj, 
+        timeEntries: newTimeEntries,
+    });
+};
+
+lunchRockData = formatHMSTimeData(lunchRockData)
+                .map(employee=>changeCompanyID(employee, lunchRockId))
+                .map(employee=>combineDuplicateDays(employee));
+nightOwlsData = formatHMSTimeData(nightOwlsData).map(employee=>changeCompanyID(employee, nightOwlsId));
+onionTechData = formatMilisecondTime(onionTechData).map(employee=>changeCompanyID(employee,onionTechId));
+gizmoGramData = gizmoGramData.map(employee=>changeCompanyID(employee, gizmoGramId));
 
 // Write the reformatted data to the output JSON file
 
-console.log(onionTechDataReformatted[0].timeEntries);
+console.log(onionTechData[0]);
 
 const lunchRockOutput = 'mockData/LunchRock_LLC-time-entries-reformatted.json';
-const lunchRockNew = JSON.stringify(lunchRockReformatted);
-fs.writeFileSync(lunchRockOutput, lunchRockNew);
+fs.writeFileSync(lunchRockOutput, JSON.stringify(lunchRockData));
 
 const nightOwlsOutput = 'mockData/Night_Owls_Inc-time-entries-reformatted.json';
-const nightOwlsNew = JSON.stringify(nightOwlsReformatted);
-fs.writeFileSync(nightOwlsOutput, nightOwlsNew);
+fs.writeFileSync(nightOwlsOutput, JSON.stringify(nightOwlsData));
 
 const onionTechOutput = 'mockData/Onion_Technology-time-entries-reformatted.json';
-const onionTechNew = JSON.stringify(onionTechDataReformatted);
-fs.writeFileSync(onionTechOutput, onionTechNew);
+fs.writeFileSync(onionTechOutput, JSON.stringify(onionTechData));
 
+const gizmoGramOutput = 'mockData/GizmoGram-time-entries-reformatted.json';
+fs.writeFileSync(gizmoGramOutput, JSON.stringify(gizmoGramData));
