7ee4655f8b96d8e57def236ce318e994d81ed18a
Mona Lisa <atdykema@gmail.com>
2023-03-29 14:36:30 -0400

    added css to manager page, .toLowerCase search term for easier search

--- client/src/components/EmployeeTable.js
+++ client/src/components/EmployeeTable.js
@@ -1,15 +1,17 @@
+import './EmployeeTable.css'
+
 const EmployeeTable = ({ employeeObjs }) => {
   // TODO: If employeeObjs is empty, because the useEffect
   //       hasn't been retrived, it will be empty at first.
   //       Possible may be able to use a sentinel value to
   //       indicate that it's not ready to render.
-  return <div>
+  return <div className='table-container'>
     <table>
       <tbody>
       {
         employeeObjs.map(employeeObj => {
           console.log(employeeObj)
-          return <tr key={employeeObj.employeeId}>
+          return <tr className='table-item' key={employeeObj.employeeId}>
             <th>
               {employeeObj.employeeId}
             </th>
