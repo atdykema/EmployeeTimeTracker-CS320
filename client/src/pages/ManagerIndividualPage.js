db8ae32512acf03de81421e5cd2c108994a14342
Mona Lisa <atdykema@gmail.com>
2023-04-05 14:09:59 -0400

    add logout buttons, back buttons, fix some page css

--- client/src/pages/ManagerIndividualPage.js
+++ client/src/pages/ManagerIndividualPage.js
@@ -1,14 +1,17 @@
 import { useState } from 'react'
 import NavigationTab from '../components/NavigationTab'
 import BarGraph from '../components/BarGraph'
-const ManagerIndividualPage = ({ pageUpdater, employeeData, subordinateData }) => {
+import LogoutButton from '../components/LogoutButton'
+const ManagerIndividualPage = ({ pageUpdater, employeeData, subordinateData, employeeDataUpdater }) => {
   const [graphDisplayOption, setGraphDisplayOption] = useState('D')
   const setDaily = (e) => setGraphDisplayOption('D')
   const setMonthly = (e) => setGraphDisplayOption('M')
   const setYearly = (e) => setGraphDisplayOption('Y')
   console.log(subordinateData)
   return <div className='page-container'>
+        <LogoutButton pageUpdater={pageUpdater} employeeDataUpdater={employeeDataUpdater}/>
         {employeeData.isManager && <NavigationTab pageUpdater={pageUpdater}/>}
+        <div className='back-button' onClick={() => pageUpdater(2)}>Back</div>
         <div className='date-info-container'>
           <h1>
             {subordinateData.firstName + ' ' + subordinateData.lastName}
