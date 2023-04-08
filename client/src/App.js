d5f600086a7218ef29766fd59325a4c0d2f1cf40
Thomas <thomasjji14@gmail.com>
2023-04-08 15:02:06 -0400

    rebase PR

--- client/src/App.js
+++ client/src/App.js
@@ -9,13 +9,12 @@ import ErrorPage from './pages/ErrorPage'
 const App = () => {
   const [employeeData, setEmployeeData] = useState({})
   const [subordinateData, setSubordinateData] = useState({})
-  // errorElement={ErrorPage}
   return (
     <Routes>
       <Route path='/' element={<Login employeeDataUpdater={setEmployeeData}/>}/>
       <Route path='/time' element={<EmployeePage employeeData={employeeData} employeeDataUpdater={setEmployeeData}/>}/>
       <Route path='/manager/view' element={<ManagerPage employeeData={employeeData} employeeDataUpdater={setEmployeeData} subordinateUpdater={setSubordinateData}/>}/>
-      <Route path='/manager/view/id' element={<ManagerIndividualPage employeeData={employeeData} subordinateData={subordinateData}/>}/>
+      <Route path='/manager/view/id' element={<ManagerIndividualPage employeeData={employeeData} employeeDataUpdater={setEmployeeData} subordinateData={subordinateData}/>}/>
       <Route path='*' element={<ErrorPage/>}/>
     </Routes>
   )
