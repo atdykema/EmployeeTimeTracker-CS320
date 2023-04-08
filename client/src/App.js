a9e1eec360185be12a97e9646a9a3379588021ec
Thomas <thomasjji14@gmail.com>
2023-04-08 14:41:34 -0400

    add error page

--- client/src/App.js
+++ client/src/App.js
@@ -4,17 +4,19 @@ import Login from './pages/Login'
 import EmployeePage from './pages/EmployeePage'
 import ManagerPage from './pages/ManagerPage'
 import ManagerIndividualPage from './pages/ManagerIndividualPage'
+import ErrorPage from './pages/ErrorPage'
 
 const App = () => {
   const [employeeData, setEmployeeData] = useState({})
   const [subordinateData, setSubordinateData] = useState({})
-
+  // errorElement={ErrorPage}
   return (
     <Routes>
-      <Route exact path='/' element={<Login employeeDataUpdater={setEmployeeData}/>} />
-      <Route exact path='/time' element={<EmployeePage employeeData={employeeData} employeeDataUpdater={setEmployeeData}/>}/>
-      <Route exact path='/manager/view' element={<ManagerPage employeeData={employeeData} employeeDataUpdater={setEmployeeData} subordinateUpdater={setSubordinateData}/>}/>
-      <Route exact path='/manager/view/id' element={<ManagerIndividualPage employeeData={employeeData} subordinateData={subordinateData}/>}/>
+      <Route path='/' element={<Login employeeDataUpdater={setEmployeeData}/>}/>
+      <Route path='/time' element={<EmployeePage employeeData={employeeData} employeeDataUpdater={setEmployeeData}/>}/>
+      <Route path='/manager/view' element={<ManagerPage employeeData={employeeData} employeeDataUpdater={setEmployeeData} subordinateUpdater={setSubordinateData}/>}/>
+      <Route path='/manager/view/id' element={<ManagerIndividualPage employeeData={employeeData} subordinateData={subordinateData}/>}/>
+      <Route path='*' element={<ErrorPage/>}/>
     </Routes>
   )
 }
---
+++ client/src/pages/ErrorPage.js
@@ -0,0 +1,9 @@
+const ErrorPage = () => {
+  return (
+    <div>
+      Page requested is not found. Try again.
+    </div>
+  )
+}
+
+export default ErrorPage
