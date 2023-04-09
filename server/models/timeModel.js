057c44346d6d980a7dea37ccfa68b319820abeb2
Nam Nguyen <tienvietnam2002@gmail.com>
2023-04-09 18:58:35 -0400

    Finish week, month, year, and <blank> option
    - Functional user/time
    - Needs testing

--- server/models/timeModel.js
+++ server/models/timeModel.js
@@ -1,16 +1,16 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const timeEntrySchema = new Schema({
-  "time" : {type: Number, required: true},
-  "date" : {type: String, required: true},
-});
+
 
 const timeSchema = new Schema({
     "companyId" : {type: Number, required: true},
     "employeeId" : {type: Number, required: true},
-    "timeEntries" : {type: [timeEntrySchema], required: true},
-    // "timeEntries" : {any: Schema.Types.Mixed, required: true},
+    // "timeEntries" : {type: [timeEntrySchema], required: true},
+    "timeEntries" : [{
+      "date" : String,
+      "hoursWorked" : Number,
+    }],
   });
 
 
