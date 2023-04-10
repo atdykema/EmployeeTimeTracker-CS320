c5542cb3e11515cc364c67330af46d153fe74bf8
Thomas Ji <bankerice@gmail.com>
2023-04-10 12:01:22 -0400

    Revert "Revert "errorpage css""

--- client/src/pages/ErrorPage.js
+++ client/src/pages/ErrorPage.js
@@ -1,7 +1,19 @@
+import './ErrorPage.css'
+import { useNavigate } from 'react-router-dom'
+import logo from './dancing-gopher.gif'
+
 const ErrorPage = () => {
+  const navigator = useNavigate()
   return (
-    <div>
-      Page requested is not found. Try again.
+    <div className="error-container">
+      <div className="error-words">
+        Whoops!
+      </div>
+      <img className = 'secret-gopher' src={logo} alt='gopher'></img>
+      <div className="error-words">
+        Page requested is not found.
+      </div>
+      <div className='back-button' onClick={() => navigator('/')}>Back to safety</div>
     </div>
   )
 }
