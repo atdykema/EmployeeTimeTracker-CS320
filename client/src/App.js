89974d401450401d24c4835dd83da62e4c1b5ed0
Varun <vgandhi@umass.edu>
2023-04-14 23:38:17 -0400

    app

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,21 @@
-import { useState } from 'react'
-import { Navigate, Route, Routes } from 'react-router-dom'
+import { Route, Routes } from 'react-router-dom'
 import Login from './pages/Login'
 import EmployeePage from './pages/EmployeePage'
 import ManagerPage from './pages/ManagerPage'
 import ManagerIndividualPage from './pages/ManagerIndividualPage'
 import ServerDownPage from './pages/ServerDownPage'
 import ErrorPage from './pages/ErrorPage'
-import { useEffect } from 'react'
-import { Cookies, useCookies } from 'react-cookie'
+import { useCookies } from 'react-cookie'
 
 const App = () => {
   const [cookies, setCookie, removeCookie] = useCookies(['data', 'subData'])
-  const [subordinateData, setSubordinateData] = useState({})
 
   return (
     <Routes>
       <Route path='/' element={<Login cookies = {cookies} cookieSetter = {setCookie}/>}/>
       <Route path='/time' element={<EmployeePage employeeData={cookies.data} employeeDataUpdater = {setCookie} cookieReset={removeCookie}/>}/>
-      <Route path='/manager/view' element={<ManagerPage employeeData={cookies.data} employeeDataUpdater={setCookie} subordinateUpdater={setSubordinateData} cookieReset={removeCookie}/>}/>
-      <Route path='/manager/view/id' element={<ManagerIndividualPage employeeData={cookies.data} employeeDataUpdater={setCookie} subordinateData={subordinateData} cookieReset={removeCookie}/>}/>
+      <Route path='/manager/view' element={<ManagerPage employeeData={cookies.data} employeeDataUpdater={setCookie} cookieReset={removeCookie}/>}/>
+      <Route path='/manager/view/id' element={<ManagerIndividualPage employeeData={cookies.data} employeeDataUpdater={setCookie} subordinateData={cookies.subData} cookieReset={removeCookie}/>}/>
       <Route path='/serverdown' element={<ServerDownPage/>}/>
       <Route path='*' element={<ErrorPage/>}/>
     </Routes>
