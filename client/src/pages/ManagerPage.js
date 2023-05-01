9d31e31ba0cf4057bd4f02e2adad56e9e0448115
Thomas <thomasjji14@gmail.com>
2023-05-01 15:39:53 -0400

    shut up eslint

--- client/src/pages/ManagerPage.js
+++ client/src/pages/ManagerPage.js
@@ -1,20 +1,20 @@
 import { useState, useEffect } from 'react'
-import EmployeeTable from '../components/EmployeeTable'
-import EmployeeSearch from '../components/EmployeeSearch'
+// import EmployeeTable from '../components/EmployeeTable'
+// import EmployeeSearch from '../components/EmployeeSearch'
 import LogoutButton from '../components/LogoutButton'
 import requests from '../services/requests'
-import loadingLogo from './loading.svg'
+// import loadingLogo from './loading.svg'
 import './ManagerPage.css'
-// import NavigationTab from '../components/NavigationTab'
+import NavigationTab from '../components/NavigationTab'
 import { useNavigate } from 'react-router-dom'
-import AggregateHistoryWindow from '../components/AggregateHistoryWindow'
+import AggregateHistoryWindow from '../components/PaymentHistoryWindow'
 
 const Managerpage = ({ employeeData, employeeDataUpdater, cookieReset, cookies }) => {
   const [isListPresent, setListPresence] = useState(false)
   // call useState on employeeObjs to be updated in useEffect
-  const [employeeObjs, setEmployeeObjs] = useState([])
-  const [searchText, updateSearchText] = useState('')
-  const [loaded, updateLoad] = useState(0)
+  // const [employeeObjs, setEmployeeObjs] = useState([])
+  // const [searchText, updateSearchText] = useState('')
+  // const [loaded, updateLoad] = useState(0)
   const navigator = useNavigate()
 
   useEffect(() => {
@@ -30,9 +30,10 @@ const Managerpage = ({ employeeData, employeeDataUpdater, cookieReset, cookies }
   //       HTTP calls must be asynchronous
   useEffect(() => {
     const fetchData = async () => {
-      updateLoad(0)
+      // updateLoad(0)
       let result
       try {
+        // eslint-disable-next-line no-unused-vars
         result = await requests.getManagerViewData(
           employeeData.employeeId,
           employeeData.companyName,
@@ -45,43 +46,42 @@ const Managerpage = ({ employeeData, employeeDataUpdater, cookieReset, cookies }
           navigator('/serverdown')
         }
       }
-      setEmployeeObjs(result.data.value) // update data
-      updateLoad(1)
+      // setEmployeeObjs(result.data.value) // update data
+      // updateLoad(1)
     }
     fetchData()
   }, [employeeData]) // runs on first render
 
-  const filterEmployees = (employees, text) => {
-    // return all employees if text is empty
-    if (text === '') {
-      return employees
-    }
+  // const filterEmployees = (employees, text) => {
+  //   // return all employees if text is empty
+  //   if (text === '') {
+  //     return employees
+  //   }
 
-    // otherwise check if text is in the ID or name
-    // currently assumes that the manager will type
-    // in first name, then last name, but can change
-    const hasText = (txt) => (employee) => {
-      return employee.employeeId.toString().startsWith(txt) || (employee.firstName + ' ' + employee.lastName).toLowerCase().startsWith(txt.toLowerCase())
-    }
-    return employees.filter(hasText(text))
-  }
+  //   // otherwise check if text is in the ID or name
+  //   // currently assumes that the manager will type
+  //   // in first name, then last name, but can change
+  //   const hasText = (txt) => (employee) => {
+  //     return employee.employeeId.toString().startsWith(txt) || (employee.firstName + ' ' + employee.lastName).toLowerCase().startsWith(txt.toLowerCase())
+  //   }
+  //   return employees.filter(hasText(text))
+  // }
 
-  const loadFunction = () => {
-    if (!loaded) {
-      return <img src={loadingLogo}></img>
-    } else {
-      return <div><EmployeeSearch text={searchText} updateText={updateSearchText} />
-      <EmployeeTable employeeObjs={filterEmployees(employeeObjs, searchText)} selectionUpdater={employeeDataUpdater} /></div>
-    }
-  }
+  // const loadFunction = () => {
+  //   if (!loaded) {
+  //     return <img src={loadingLogo}></img>
+  //   } else {
+  //     return <div><EmployeeSearch text={searchText} updateText={updateSearchText} />
+  //     <EmployeeTable employeeObjs={filterEmployees(employeeObjs, searchText)} selectionUpdater={employeeDataUpdater} /></div>
+  //   }
+  // }
 
   return (cookies.data === undefined)
     ? <div/>
     : (
         <div className='page-container'>
-        {/* {employeeData.isManager && <NavigationTab />} */}
+        {employeeData.isManager && <NavigationTab />}
         <LogoutButton employeeDataUpdater={employeeDataUpdater} cookieReset= {cookieReset}/>
-        {loadFunction()}
         <AggregateHistoryWindow isListPresent={isListPresent} setListPresence={setListPresence} employeeData={employeeData}/>
         </div>
       )
